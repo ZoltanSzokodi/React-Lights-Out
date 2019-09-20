54f0cf3deb0b0fbc25a145ff20a286bf64fbf86a
Zoltan Szokodi <zoltan.szokodi@hotmail.com>
2019-09-20 13:41:38 +0200

    Styling

---
+++ build/precache-manifest.6bb6a4aa9982b10141238f65aa51f224.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a9f2a95641202e2f76f9",
+    "url": "/static/js/main.a9f2a956.chunk.js"
+  },
+  {
+    "revision": "03109274422a2c9f0101",
+    "url": "/static/js/1.03109274.chunk.js"
+  },
+  {
+    "revision": "a9f2a95641202e2f76f9",
+    "url": "/static/css/main.0e124a32.chunk.css"
+  },
+  {
+    "revision": "0d650e608e412ebdf3be99be910629fd",
+    "url": "/index.html"
+  }
+];
--- src/Board.js
+++ src/Board.js
@@ -35,7 +35,6 @@ class Board extends Component {
   }
 
   /** handle changing a cell: update board & determine if winner */
-
   flipCellsAround(coord) {
     console.log("FLIPPING", coord);
 
@@ -71,7 +70,14 @@ class Board extends Component {
 
   render() {
     if (this.state.hasWon) {
-      return <h1>YOU WON!!!</h1>
+      return (
+        <div className="Board-title">
+          <div className="winner">
+            <span className="neon-orange">YOU</span>
+            <span className="neon-blue">WIN!</span>
+          </div>
+        </div>
+      )
     }
 
     let tblBoard = [];
@@ -87,11 +93,17 @@ class Board extends Component {
       tblBoard.push(<tr key={y}>{row}</tr>)
     }
     return (
-      <table className="Board">
-        <tbody>
-          {tblBoard}
-        </tbody>
-      </table>
+      <div>
+        <div className="Board-title">
+          <div className="neon-orange">Lights</div>
+          <div className="neon-blue">Out</div>
+        </div>
+        <table className="Board">
+          <tbody>
+            {tblBoard}
+          </tbody>
+        </table>
+      </div>
     )
   }
 }
